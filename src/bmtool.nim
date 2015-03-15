aa15c3074774f0957d0eab2813420f974643377a
Wink Saville <wink@saville.com>
2015-03-15 10:19:11 -0700

    Two versions of benchmark doBmTicks and doBmTime.
    doBmTicks uses getTick from timers.nim which is from
    nim/lib/pure/timers.nim, it wasn't public so I copied it.
    
    doBmTime uses epochTime() and is doBenchmark renamed.
    
    Also, the RunningStat that's returned is based on time per loop as
    I thought that would be more convienent.

--- src/bmtool.nim
+++ src/bmtool.nim
@@ -1,14 +1,30 @@
-import times, math
+import timers,math
 
-template doBenchmark*(benchmarkExpression: expr, loops: int, innerLoops: int): RunningStat =
+template doBmTicks*(benchmarkExpression: expr, loops: int, innerLoops: int): RunningStat =
+  var result: RunningStat
+  for idx in 0..loops-1:
+    var st = getTicks()
+    for innerIdx in 0..innerLoops-1:
+      benchmarkExpression
+    var et = getTicks()
+    var duration = float(et - st)
+    if duration < 0:
+      echo "bad duration=" & $duration & " et=" & $float(et) & " st=" & $float(st)
+    else:
+      result.push(duration/float(innerLoops))
+  result
+
+template doBmTime*(benchmarkExpression: expr, loops: int, innerLoops: int): RunningStat =
   var result: RunningStat
   for idx in 0..loops-1:
     var st = epochTime()
     for innerIdx in 0..innerLoops-1:
       benchmarkExpression
     var et = epochTime()
-    var duration = et - st
-    var durationPerInnerLoop = duration/toFloat(innerLoops)
-    result.push(durationPerInnerLoop)
+    var duration = float(et - st)
+    if duration < 0:
+      echo "bad duration=" & $duration & " et=" & $float(et) & " st=" & $float(st)
+    else:
+      result.push(duration/float(innerLoops))
   result
 
--- tests/test1.nim
+++ tests/test1.nim
@@ -11,16 +11,36 @@ var
 proc x(v: int) =
   i += v
 
+proc y() =
+  discard
+
 proc `$`*(r: RunningStat): string =
   "{n=" & $r.n & " sum=" & $r.sum & " min=" & $r.min & " max=" & $r.max & " mean=" & $r.mean & "}"
 
 
-for z in 1..8:
+for z in 1..6:
   innerLoops *= 10
   var
-    stats = doBenchmark(x(2), 100, innerLoops)
+    empty = doBmTicks(y(), 100, innerLoops)
+    stats = doBmTicks(x(2), 100, innerLoops)
 
-  echo "standardDeviation=" & $stats.standardDeviation() & " innerLoops=" & $innerLoops & " stats=" & $stats
+  echo "empty standardDeviation=" & $empty.standardDeviation() & " innerLoops=" & $innerLoops & " empty=" & $empty
+  echo "stats standardDeviation=" & $stats.standardDeviation() & " innerLoops=" & $innerLoops & " stats=" & $stats
+  echo "min diff=" & $(stats.min - empty.min)
+
+innerLoops = 1
+
+echo ""
+
+for z in 1..6:
+  innerLoops *= 10
+  var
+    empty = doBmTime(y(), 100, innerLoops)
+    stats = doBmTime(x(2), 100, innerLoops)
+
+  echo "empty standardDeviation=" & $empty.standardDeviation() & " innerLoops=" & $innerLoops & " empty=" & $empty
+  echo "stats standardDeviation=" & $stats.standardDeviation() & " innerLoops=" & $innerLoops & " stats=" & $stats
+  echo "min diff=" & $(stats.min - empty.min)
 
 echo "test1:-"
 
