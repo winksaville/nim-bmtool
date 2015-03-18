2d538b5b9a25f72b02492db226f06d9818e54595
Wink Saville <wink@saville.com>
2015-03-17 22:21:37 -0700

    Added using Cycles.

--- src/bmtool.nim
+++ src/bmtool.nim
@@ -1,30 +1,111 @@
-import timers,math
+import math, timers
 
-template doBmTicks*(benchmarkExpression: expr, loops: int, innerLoops: int): RunningStat =
+proc getBegCycles*(): int64 {.inline.} =
+  var begHi, begLo: uint32
+  asm """
+    mfence
+    rdtsc
+    :"=a"(`begLo`), "=d"(`begHi`)
+  """
+  result = int64(begLo) or (int64(begHi) shl 32)
+
+proc getEndCycles*(): int64 {.inline.} =
+  var endHi, endLo: uint32
+  asm """
+    rdtscp
+    mfence
+    :"=a"(`endLo`), "=d"(`endHi`)
+  """
+  result = int64(endLo) or (int64(endHi) shl 32)
+
+proc measureCycles*(procedure: proc()): int64 =
+  ## Returns number of cycles to execute the procedure parameter.
+  ## It uses rdtsc/rdtscp and mfence to measure the execution
+  ## time as described the Intel paper titled "How to Benchmark
+  ## Code Execution Times on Intel IA-32 and IA-64 Instruction Set" 
+  var begHi, begLo: uint32
+  var endHi, endLo: uint32
+  var begCycles, endCycles: int64
+  asm """
+    mfence
+    rdtsc
+    :"=a"(`begLo`), "=d"(`begHi`)
+  """
+  procedure()
+  asm """
+    rdtscp
+    mfence
+    :"=a"(`endLo`), "=d"(`endHi`)
+  """
+  begCycles = int64(begLo) or (int64(begHi) shl 32)
+  endCycles = int64(endLo) or (int64(endHi) shl 32)
+  result = endCycles - begCycles
+
+template doBmCycles*(procedure: proc(), loops: int, innerLoops: int): RunningStat =
+  # Uses measureCycles to return the RunningStat of executing the procedure parameter.
+  # Since measureCycles has asm statements that don't expand properly in templates we
+  # have to use a proc procedure. This is quite a bit less flexible then being able to
+  # pass an expression but yields probably the second best results but more testing needed.
   var result: RunningStat
   for idx in 0..loops-1:
-    var st = getTicks()
     for innerIdx in 0..innerLoops-1:
+      var cycles = measureCycles(procedure)
+      if cycles < 0:
+        echo "bad cycles=", cycles
+      else:
+        result.push(float(cycles))
+  result
+
+template doBmCycles2*(benchmarkExpression: expr, loops: int, innerLoops: int): RunningStat =
+  # Uses getBegCycles/getEndCycles to get the cycles and returns the RunningStat.
+  # Since the asm statements are in a procudure we can use a benchmarkExpression parameter
+  # This isn't quite as good as doBmCycles2 or doBmTicks its still pretty good, we'll
+  # have to see how it does on the linux box. And hopefully Araq will be inclined to fix
+  # the bug and we'll be able to use asm statements here in a template.
+  var result: RunningStat
+  for idx in 0..loops-1:
+    for innerIdx in 0..innerLoops-1:
+      var st = getBegCycles()
       benchmarkExpression
-    var et = getTicks()
-    var duration = float(et - st)
-    if duration < 0:
-      echo "bad duration=" & $duration & " et=" & $float(et) & " st=" & $float(st)
-    else:
-      result.push(duration/float(innerLoops))
+      var et = getEndCycles()
+      var duration = float(et - st)
+      if duration < 0:
+        echo "bad duration=" & $duration & " et=" & $float(et) & " st=" & $float(st)
+      else:
+        result.push(duration)
+  result
+
+template doBmTicks*(benchmarkExpression: expr, loops: int, innerLoops: int): RunningStat =
+  # Uses getTicks to return the RunningStat of executing the benchmarkExpression parameter.
+  # On the mac this uses  mac specific and yeilds probably the most consistent results.
+  var result: RunningStat
+  for idx in 0..loops-1:
+    for innerIdx in 0..innerLoops-1:
+      var st = getTicks()
+      benchmarkExpression
+      var et = getTicks()
+      var duration = float(et - st)
+      if duration < 0:
+        echo "bad duration=" & $duration & " et=" & $float(et) & " st=" & $float(st)
+      else:
+        result.push(duration)
   result
 
 template doBmTime*(benchmarkExpression: expr, loops: int, innerLoops: int): RunningStat =
+  ## Use epochTime to measure. Since one inner loop is to short of a time frame
+  ## we measure the time for all of the inner loops then divide by number of
+  ## innerLoops to get time per loop. Overall time gives the poorest result
+  ## because of the lack of enough resolution relative to ticks or cycles.
   var result: RunningStat
   for idx in 0..loops-1:
     var st = epochTime()
     for innerIdx in 0..innerLoops-1:
       benchmarkExpression
     var et = epochTime()
-    var duration = float(et - st)
+    var duration = float(et - st)/float(innerLoops)
     if duration < 0:
       echo "bad duration=" & $duration & " et=" & $float(et) & " st=" & $float(st)
     else:
-      result.push(duration/float(innerLoops))
+      result.push(duration)
   result
 
--- tests/test1.nim
+++ tests/test1.nim
@@ -5,42 +5,86 @@ echo "test1:+"
 var
   i = 1
 
+const
+  outerLoops = 1000
+
 var
   innerLoops = 1
-
-proc x(v: int) =
-  i += v
 
 proc y() =
   discard
 
+var
+  zv: int
+proc zproc() =
+  zv += 1
+
 proc `$`*(r: RunningStat): string =
   "{n=" & $r.n & " sum=" & $r.sum & " min=" & $r.min & " max=" & $r.max & " mean=" & $r.mean & "}"
 
+var
+  empty: RunningStat
+  stats: RunningStat
 
-for z in 1..4:
-  innerLoops *= 10
-  var
-    empty = doBmTicks(y(), 10000, innerLoops)
-    stats = doBmTicks(x(2), 10000, innerLoops)
+var
+  cy = measureCycles(y)
+echo "measureCyclesExpr(y)=", cy
 
-  echo "empty standardDeviation=" & $empty.standardDeviation() & " innerLoops=" & $innerLoops & " empty=" & $empty
-  echo "stats standardDeviation=" & $stats.standardDeviation() & " innerLoops=" & $innerLoops & " stats=" & $stats
-  echo "min diff=" & $(stats.min - empty.min)
+var rs = doBmCycles(y, outerLoops, innerloops)
+echo "doBmCycles(y)=", rs
 
 innerLoops = 1
+for z in 1..4:
+  innerLoops *= 10
+
+  empty = doBmCycles(y, outerLoops, innerLoops)
+  stats = doBmCycles(zproc, outerLoops, innerLoops)
+
+  echo "cycles empty standardDeviation=" & $empty.standardDeviation() & " innerLoops=" & $innerLoops & " empty=" & $empty
+  echo "cycles stats standardDeviation=" & $stats.standardDeviation() & " innerLoops=" & $innerLoops & " stats=" & $stats
+  echo "cycles min diff=" & $(stats.min - empty.min)
+
 
 echo ""
 
-for z in 1..5:
+innerLoops = 1
+for z in 1..4:
+  innerLoops *= 10
+
+  empty = doBmCycles2(y(), outerLoops, innerLoops)
+  stats = doBmCycles2(zproc(), outerLoops, innerLoops)
+
+  echo "cycles2 empty standardDeviation=" & $empty.standardDeviation() & " innerLoops=" & $innerLoops & " empty=" & $empty
+  echo "cycles2 stats standardDeviation=" & $stats.standardDeviation() & " innerLoops=" & $innerLoops & " stats=" & $stats
+  echo "cycles2 min diff=" & $(stats.min - empty.min)
+
+
+echo ""
+
+innerLoops = 1
+for z in 1..4:
+  innerLoops *= 10
+
+  empty = doBmTicks(y(), outerLoops, innerLoops)
+  stats = doBmTicks(zproc(), outerLoops, innerLoops)
+
+  echo "ticks empty standardDeviation=" & $empty.standardDeviation() & " innerLoops=" & $innerLoops & " empty=" & $empty
+  echo "ticks stats standardDeviation=" & $stats.standardDeviation() & " innerLoops=" & $innerLoops & " stats=" & $stats
+  echo "ticks min diff=" & $(stats.min - empty.min)
+
+
+echo ""
+
+innerLoops = 1
+for z in 1..4:
   innerLoops *= 10
   var
-    empty = doBmTime(y(), 10000, innerLoops)
-    stats = doBmTime(x(2), 10000, innerLoops)
+    empty = doBmTime(y(), outerLoops, innerLoops)
+    stats = doBmTime(zproc(), outerLoops, innerLoops)
 
-  echo "empty standardDeviation=" & $empty.standardDeviation() & " innerLoops=" & $innerLoops & " empty=" & $empty
-  echo "stats standardDeviation=" & $stats.standardDeviation() & " innerLoops=" & $innerLoops & " stats=" & $stats
-  echo "min diff=" & $(stats.min - empty.min)
+  echo "time empty standardDeviation=" & $empty.standardDeviation() & " innerLoops=" & $innerLoops & " empty=" & $empty
+  echo "time stats standardDeviation=" & $stats.standardDeviation() & " innerLoops=" & $innerLoops & " stats=" & $stats
+  echo "time min diff=" & $(stats.min - empty.min)
 
 echo "test1:-"
 
